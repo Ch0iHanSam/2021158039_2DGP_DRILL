ffe4a2bf3db244942e12b5e8f053b33182bd171a
Ch0iHanSam <112991925+Ch0iHanSam@users.noreply.github.com>
2022-09-28 12:18:55 +0900

    drill07 start

---
+++ 06/move_character_ver_professer.py
@@ -0,0 +1,48 @@
+from pico2d import *
+import math
+
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def render_all(x,y):
+     clear_canvas_now()
+     grass.draw_now(400,30)
+     character.draw_now(x,y)
+     delay(0.01)
+        
+def run_circle():
+    
+    cx , cy, cr= 400, 300, 200
+    for deg in range(0,360,5):
+        x = cx+cr*math.cos(deg/360*2*math.pi)
+        y = cy+cr*math.sin(deg/360*2*math.pi)
+        render_all(x,y)
+    
+
+def run_rectangle():
+
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    for x  in range(50,750+1,10):#변 움직임
+        render_all(x,90)
+
+    for y in range(90,510+1,10):
+        render_all(750,y)
+
+    for x in range(750,50,-10):
+        render_all(x,510)
+
+    for y in range(510,90,-10):
+        render_all(50,y)
+
+while True:
+    run_circle()
+    run_rectangle()
+
+
+
+close_canvas()
+
