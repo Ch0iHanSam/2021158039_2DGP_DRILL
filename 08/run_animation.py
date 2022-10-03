0f7b4b8b0149c4ed1c0f6377212aa638021d3187
Ch0iHanSam <112991925+Ch0iHanSam@users.noreply.github.com>
2022-10-03 13:02:24 +0900

    restart

--- 08/run_animation.py
+++ 08/run_animation.py
@@ -3,12 +3,12 @@ from pico2d import *
 open_canvas()
 grass = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
-dir = 100
+dir = 300
 running = True
 x = 800//2
 y = 150
 frame = 0
+
 
 def handle_events():
     global running
@@ -32,12 +32,17 @@ def handle_events():
                 y = y + 10
             elif event.key == SDLK_DOWN:
                 y = y - 10
-
+        if event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir = 300
+            elif event.key == SDLK_LEFT:
+                dir = 200
 
 
 while running:
     clear_canvas()
     grass.draw(400,300,800,600)
+
 
     if x > 30 and x < 770 and y > 30 and y < 570:
         character.clip_draw(frame * 100, dir, 100, 100, x, y)
